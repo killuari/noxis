88fe7b996127bfaf68e942398d91820a46d7efe2
killuari <killuari@protonmail.com>
2025-06-04 20:44:46 +0200

    Add placeholder functions for InventoryManager

---
+++ inventory_manager.py
@@ -0,0 +1,14 @@
+from items import *
+
+class InventoryManager:
+    async def add_item(self):
+        pass
+
+    async def remove_item(self):
+        pass
+
+    async def get_inventory(self):
+        pass
+
+    async def get_inventory_value(self):
+        pass
