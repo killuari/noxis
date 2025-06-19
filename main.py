c1ebe45b0bbbf3bc19c05ab716275eb3f51460b8
Athanasios Giotas <148756853+AthanasiosG@users.noreply.github.com>
2025-06-19 02:59:50 +0200

    Remove # in main

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ TOKEN = os.getenv("BOT_TOKEN")
 @client.event
 async def on_ready():
     await DatabaseManager.init_database()
-    # await DatabaseManager.add_column_to_table("database.db", "last_used", "higherlower", "TIMESTAMP", None)
+    await DatabaseManager.add_column_to_table("database.db", "last_used", "higherlower", "TIMESTAMP", None)
     await client.add_cog(BasicCommands(client))    
     await client.tree.sync()
 
