ed767be8ffa94eaa40ab9852e5bed797836470d0
killuari <killuari@protonmail.com>
2025-06-29 21:52:04 +0200

    Test hook

--- main.py
+++ main.py
@@ -32,4 +32,5 @@ async def on_guild_join(guild: discord.Guild):
 async def on_member_join(member):
     await member.send(embed=discord.Embed(title="Welcome on this Server!", description="Click on **Get Started** to create an account in order to be able to start playing!", color=discord.Color.green()), view=GetStarted())
  
+ 
 client.run(TOKEN)
